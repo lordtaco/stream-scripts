1c30f9b83d0f2d2e7b5babcd63ee580bfad4a100
Russ Jackson <rjackson@catt.com>
2021-10-18 15:59:31 -0400

    Update colorchange.php
    Adding a random option and allowing more special characters in color name.

--- colorchange.php
+++ colorchange.php
@@ -1,57 +1,45 @@
 <?php
-
-define('GOVEE_API_KEY', 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
-define('GOVEE_API_URL', 'https://developer-api.govee.com/v1/devices/control');
-define('GOVEE_MODEL', 'H6159');
-define('GOVEE_SHELF_LIGHTS', '00:00:00:00:00:00:00:00');
-define('GOVEE_CONSOLE_LIGHTS', '00:00:00:00:00:00:00:00');
-
+require_once('config.php');
+require_once('functions.php');
 require_once('colors.php');
 
-$color = preg_replace("/[^a-z\ ]+/", "", strtolower($_GET['color']));
+$color_found = 0;
+$chosen_color = "";
+$color = preg_replace("/[^a-z\ ()\/&\-\'’]+/", "", strtolower($_GET['color']));
 
 if(array_key_exists($color, $colors) === true)
 {
-	$red = $colors[$color][0];
-	$green = $colors[$color][1];
-	$blue = $colors[$color][2];
+	$chosen_color = $color;	
+	$color_found = 1;
 	
-	setGoveeLights(GOVEE_SHELF_LIGHTS, $red, $green, $blue);
-	setGoveeLights(GOVEE_CONSOLE_LIGHTS, $red, $green, $blue);
-	echo 'Lights changed to ' . $color; 
-	if(isset($_GET['username']) && $_GET['username'] != '')
-	{
-		echo ' by ' . $_GET['username'] . '.';
-	}
+} elseif($color == "random") {
+	$chosen_color = array_rand($colors);	
+	$color_found = 1;
 } else {
 	echo 'Color ' . $color . ' not found.';	
 }
 
-function setGoveeLights($device, $red, $green, $blue)
+if($color_found == 1)
 {
-	
-	$curl = curl_init();	
-	curl_setopt_array($curl, array(
-	CURLOPT_URL => GOVEE_API_URL,
-	CURLOPT_RETURNTRANSFER => true,
-	CURLOPT_ENCODING => "",	
-	CURLOPT_MAXREDIRS => 10,
-  	CURLOPT_TIMEOUT => 30,
-  	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  	CURLOPT_CUSTOMREQUEST => "PUT",
-  	CURLOPT_POSTFIELDS => '{ "device": "' . $device . '", "model": "' . GOVEE_MODEL . '", "cmd": { "name": "color", "value": { "r": ' . $red . ', "g": ' . $green . ', "b": ' . $blue . ' }}}',
-	CURLOPT_HTTPHEADER => array(
-    	"cache-control: no-cache",
-    	"Content-Type: application/json",
-    	"Govee-API-Key: " . GOVEE_API_KEY
-  		),
-	));
+	$red = $colors[$chosen_color][0];
+	$green = $colors[$chosen_color][1];
+	$blue = $colors[$chosen_color][2];
 
-	$response = curl_exec($curl);
-	$err = curl_error($curl);
-
-	curl_close($curl);
+	if(isset($_GET['save']) && $_GET['save'] == 'true')
+	{
+		$conn = new mysqli(MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB);	
+		mysqli_query($conn, "update `" . TABLE_NAME . "` set red = " . $red . ", green = " . $green . ", blue = " . $blue . " where id = 1"); 
+		mysqli_close($conn);
+	}
 	
+	setGoveeLights(GOVEE_SHELF_LIGHTS, $red, $green, $blue);
+	setGoveeLights(GOVEE_CONSOLE_LIGHTS, $red, $green, $blue);
+	echo 'Lights changed to ' . $chosen_color; 
+	if(isset($_GET['username']) && $_GET['username'] != '')
+	{
+		echo ' by ' . $_GET['username'] . '.';
+	}
+
 }
 
 ?>
