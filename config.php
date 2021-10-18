d1a104d7c35773fc5ab83f460031e69fc580195a
Russ Jackson <rjackson@catt.com>
2021-10-18 16:01:20 -0400

    Create config.php
    Adding a config file to store all our keys/passwords/etc.

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+define('GOVEE_API_KEY', 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+define('GOVEE_API_URL', 'https://developer-api.govee.com/v1/devices/control');
+define('GOVEE_MODEL', 'H6159');
+define('GOVEE_SHELF_LIGHTS', 'XX:XX:XX:XX:XX:XX:XX:XX');
+define('GOVEE_CONSOLE_LIGHTS', 'XX:XX:XX:XX:XX:XX:XX:XX');
+define('MYSQL_HOST', 'localhost');
+define('MYSQL_USER', 'xxxxxxxx');
+define('MYSQL_PASS', 'xxxxxxxxxxxx');
+define('MYSQL_DB', 'xxxxxxxx');
+define('TABLE_NAME', 'color_values');
+
+?>
